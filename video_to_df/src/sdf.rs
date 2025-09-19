8a2d8d4b80c32eb8ea4fd56161e0cc9a26cac93d
Klinbee <imquidvio@gmail.com>
2025-09-19 13:51:55 -0500

    change: true two-way SDF

--- video_to_df/src/sdf.rs
+++ video_to_df/src/sdf.rs
@@ -2,44 +2,93 @@ use crate::MonoFrame;
 
 pub fn binary_sdf(frame: &MonoFrame) -> MonoFrame
 {
-    // First, compute the normal sdf
-    let sdf_raw = chebyshev_sdf_two_pass(
-        &frame.data,
-        frame.width as usize,
-        frame.height as usize,
-        127, // Splits 0-127 & 128-255
-    );
+    // Compute the above threshold and below threshold SDF
+    // Splits 0-127 & 128-255;
+    let above_distances =
+        chebyshev_sdf_above(&frame.data, frame.width as usize, frame.height as usize, 127);
+    let below_distances =
+        chebyshev_sdf_below(&frame.data, frame.width as usize, frame.height as usize, 127);
 
-    // Then, find the `max_value` in it
-    let max_value: usize = *sdf_raw.iter().max().expect("SDF Raw should never have size 0");
+    // Then, find the `max_value` in them
+    let above_max = *above_distances.iter().max().expect("SDF should never have size 0");
+    let below_max = *below_distances.iter().max().expect("SDF should never have size 0");
 
-    // If the `max_value` is 0, then use all white (as the SDF value is inverted)
-    if max_value == 0
-    {
-        return MonoFrame::new(vec![255; sdf_raw.len()], frame.width, frame.height);
-    }
-
-    // Then, convert the `sdf_raw` from `usize` to `u8` by normalizing to `max_value` and clamping
-    let sdf_bytes = sdf_raw
+    // Then, convert the `_bytes` from `usize` to `u8` by normalizing to `_max` and clamping
+    let above_bytes: Vec<u8> = above_distances
         .iter()
-        .map(|&val| {
-            let norm = 1.0 - (val as f32 / max_value as f32);
-            (norm * 255.0).round().clamp(0.0, 255.0) as u8
+        .map(|&dist| {
+            let norm = 1.0 - (dist as f32 / above_max as f32);
+            (norm * 127.0).round().clamp(0.0, 127.0) as u8
+        })
+        .collect();
+    let below_bytes: Vec<u8> = below_distances
+        .iter()
+        .map(|&dist| {
+            let norm = dist as f32 / below_max as f32;
+            128 + (norm * 127.0).round().clamp(0.0, 127.0) as u8
+        })
+        .collect();
+
+    // Then, combine them, such that the minimum `below_bytes` masks to `above_bytes`
+    let combined_bytes: Vec<u8> = below_bytes
+        .iter()
+        .zip(&above_bytes)
+        .map(|(&below, &above)| {
+            match below
+            {
+                128 => above,
+                _ => below,
+            }
         })
         .collect();
 
     // Return it as a MonoFrame
-    MonoFrame::new(sdf_bytes, frame.width, frame.height)
+    MonoFrame::new(combined_bytes, frame.width, frame.height)
 }
 
-fn chebyshev_sdf_two_pass(
+fn chebyshev_sdf_below(
     image: &[u8],
     width: usize,
     height: usize,
     threshold: u8,
 ) -> Vec<usize>
 {
-    let mut distance_field: Vec<usize> = vec![usize::MAX; width * height];
+    // max distance for chebyshev
+    let max_dist = width + height;
+
+    let mut distance_field: Vec<usize> = vec![max_dist; width * height];
+
+    // Sets the distance field value at that position to 0 where the pixel value is above threshold
+    distance_field.iter_mut().zip(image.iter()).for_each(|(dist_val, pixel_val)| {
+        if pixel_val <= &threshold
+        {
+            *dist_val = 0;
+        }
+    });
+
+    chebyshev_sdf_forward_pass(&mut distance_field, width, height);
+
+    // Better access pattern to reverse all at once and walk forward
+    distance_field.reverse();
+    chebyshev_sdf_forward_pass(&mut distance_field, width, height);
+
+    // Change to normal order
+    distance_field.reverse();
+
+    distance_field
+}
+
+fn chebyshev_sdf_above(
+    image: &[u8],
+    width: usize,
+    height: usize,
+    threshold: u8,
+) -> Vec<usize>
+{
+    // max distance for chebyshev
+    let max_dist = width + height;
+
+    let mut distance_field: Vec<usize> = vec![max_dist; width * height];
 
     // Sets the distance field value at that position to 0 where the pixel value is above threshold
     distance_field.iter_mut().zip(image.iter()).for_each(|(dist_val, pixel_val)| {
@@ -67,7 +116,7 @@ fn chebyshev_sdf_forward_pass(
     height: usize,
 )
 {
-    // Forward pass (row-wise, column-wise, diagonal-wise)
+    // Forward pass (right, bottom-right, bottom, bottom-left)
     let mut idx = 0;
     for y in 0..height
     {
@@ -75,38 +124,28 @@ fn chebyshev_sdf_forward_pass(
         {
             let mut curr_dist = distance_field[idx];
 
-            // Top-left Diagonal (if within bounds)
-
-            if x > 0 && y > 0
+            // Left (if within bounds)
+            if x != 0
             {
-                let n_dist = distance_field[idx - width - 1];
-                let new_dist = n_dist + 1;
-                if new_dist < curr_dist
-                {
-                    curr_dist = new_dist;
-                }
+                curr_dist = curr_dist.min(distance_field[idx - 1] + 1);
+            }
+
+            // Top-right Diagonal (if within bounds)
+            if (x != (width - 1)) && (y != 0)
+            {
+                curr_dist = curr_dist.min(distance_field[idx - width + 1] + 1);
             }
 
             // Top (if within bounds)
-            if y > 0
+            if y != 0
             {
-                let n_dist = distance_field[idx - width];
-                let new_dist = n_dist + 1;
-                if new_dist < curr_dist
-                {
-                    curr_dist = new_dist;
-                }
+                curr_dist = curr_dist.min(distance_field[idx - width] + 1);
             }
 
-            // Left (if within bounds)
-            if x > 0
+            // Top-left Diagonal (if within bounds)
+            if (x != 0) && (y != 0)
             {
-                let n_dist = distance_field[idx - 1];
-                let new_dist = n_dist + 1;
-                if new_dist < curr_dist
-                {
-                    curr_dist = new_dist;
-                }
+                curr_dist = curr_dist.min(distance_field[idx - width - 1] + 1);
             }
 
             distance_field[idx] = curr_dist;
